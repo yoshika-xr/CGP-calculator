1731f7d12f77948b37537931aaeb32d8e17a23a9
yoshika-xr <yoshikajune@gmail.com>
2026-05-01 18:32:45 +0530

    update styles

--- src/componant/content.jsx
+++ src/componant/content.jsx
@@ -28,7 +28,7 @@ function Content() {
   // grade → point
   const getGradePoint = (grade) => {
     const map = {
-      O: 10,
+      " O": 10,
       "A+": 9,
       A: 8,
       "B+": 7,
@@ -80,9 +80,9 @@ function Content() {
       <div className="welcome-content">
         <h1>👋 Welcome!</h1>
         <p>
-          Managing your grades doesn’t have to be difficult.<br /> This CGPA
-          Calculator helps you quickly calculate your results and stay on top of
-          your academic progress.
+          Managing your grades doesn’t have to be difficult.
+          <br /> This CGPA Calculator helps you quickly calculate your results
+          and stay on top of your academic progress.
         </p>
       </div>
       <div className="calcu-sec">
@@ -96,8 +96,19 @@ function Content() {
         ))}
       </div>
 
-      <i className="bi bi-plus-circle" onClick={addForm}></i>
-      <button
+      <i className="plusButton" onClick={addForm}>
+        <svg
+          class="plusIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+        >
+          <g mask="url(#mask0_21_345)">
+            <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+          </g>
+        </svg>
+      </i>
+
+      {/* <button
         className="calc-btn"
         onClick={() => {
           calculateGPA();
@@ -105,6 +116,24 @@ function Content() {
         }}
       >
         Calculate
+      </button> */}
+      <button
+        className="cssbuttons-io"
+        onClick={() => {
+          calculateGPA();
+          sendData();
+        }}
+      >
+        <span>
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          Calculate
+        </span>
       </button>
     </div>
   );
