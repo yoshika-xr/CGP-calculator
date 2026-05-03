5454f1430a0e8b3efd5b085aa41e90f348e6892f
yoshika-xr <yoshikajune@gmail.com>
2026-05-03 18:38:34 +0530

    update logo

--- src/componant/Header.jsx
+++ src/componant/Header.jsx
@@ -6,7 +6,35 @@ function Header() {
     <>
       <div className="header">
         <div className="left-side">
-          <img className={"logo"} src={logo} alt="logo" />
+          <i>
+            <svg width="60" height="50" viewBox="0 0 100 100">
+              <path
+                d="M70 20 A35 35 0 1 0 70 80"
+                fill="none"
+                stroke="#37cf71b4"
+                strokeWidth="8"
+                strokeLinecap="round"
+              />
+              <line
+                x1="30"
+                y1="40"
+                x2="65"
+                y2="40"
+                stroke="#353535"
+                strokeWidth="6"
+                strokeLinecap="round"
+              />
+              <line
+                x1="30"
+                y1="55"
+                x2="65"
+                y2="55"
+                stroke="#383838"
+                strokeWidth="6"
+                strokeLinecap="round"
+              />
+            </svg>
+          </i>
 
           <h2 className={"head-title $"}>EduCalc</h2>
         </div>
@@ -19,7 +47,6 @@ function Header() {
           <li>Sign</li>
         </ul>
       </div>
-     
     </>
   );
 }
